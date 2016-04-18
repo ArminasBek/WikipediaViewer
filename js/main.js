f7c927712baab1a717f1c02156164b94bf0e1948
Arminas <arminasb7@gmail.com>
2016-04-18 17:08:37 +0100

    API functionality added
    Console logs different items from array ant concats them. Need to
    automate console.log function

--- js/main.js
+++ js/main.js
@@ -1,14 +1,23 @@
 $("button#search").on("click", function(){
 	var inputVal = $("input#text").val();
 	$.getJSON("https://en.wikipedia.org/w/api.php?action=opensearch&search=" + inputVal +"&limit=5&namespace=0&format=json&callback=?", function(json) {
-		console.log(json);
-		$("input#text").val('');
-	})	
-});
+		var itemName = $.each(json[1], function(i, val){	
+		})
+		var itemDescription = $.each(json[2], function(i, val){	
+		})
+		var itemLink = $.each(json[3], function(i, val){
+		})
+		console.log(itemName[0] + " " + itemDescription[0] + " " + itemLink[0]);
+		console.log(itemName[1] + " " + itemDescription[1] + " " + itemLink[1]);
+		console.log(itemName[2] + " " + itemDescription[2] + " " + itemLink[2]);
+		console.log(itemName[3] + " " + itemDescription[3] + " " + itemLink[3]);
+		console.log(itemName[4] + " " + itemDescription[4] + " " + itemLink[4]);
+		
+
+		})//EOF getJSON
+});//EOF button click
 		
 	
-	
-
 
 
 
