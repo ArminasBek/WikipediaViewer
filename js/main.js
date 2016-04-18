ef5cf5e01a551e93c8234dd359e55809e834e92c
Arminas <arminasb7@gmail.com>
2016-04-18 18:27:53 +0100

    Finished JQuery code for full functionality

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $("button#search").on("click", function(){
+	$("div#container").empty();
 	var inputVal = $("input#text").val();
 	$.getJSON("https://en.wikipedia.org/w/api.php?action=opensearch&search=" + inputVal +"&limit=5&namespace=0&format=json&callback=?", function(json) {
 		var itemName = $.each(json[1], function(i, val){	
@@ -7,13 +8,9 @@ $("button#search").on("click", function(){
 		})
 		var itemLink = $.each(json[3], function(i, val){
 		})
-		console.log(itemName[0] + " " + itemDescription[0] + " " + itemLink[0]);
-		console.log(itemName[1] + " " + itemDescription[1] + " " + itemLink[1]);
-		console.log(itemName[2] + " " + itemDescription[2] + " " + itemLink[2]);
-		console.log(itemName[3] + " " + itemDescription[3] + " " + itemLink[3]);
-		console.log(itemName[4] + " " + itemDescription[4] + " " + itemLink[4]);
-		
-
+		for (var i=0; i <itemName.length; i++) {
+			$("div#container").append("<a target='_blank' href='" + itemLink[i] + "'><div class='item'><span class='itemName'>" + itemName[i] + "</span>" + " " + itemDescription[i] + "</div></a>");
+		}
 		})//EOF getJSON
 });//EOF button click
 		
